41e39e372b31c182b0a86ef6ce1718934e3dd3ca
Guitti <guilhermederetti9@gmail.com>
2022-12-13 12:25:29 +0000

    finished converting html to Jquerry, tested and working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,9 @@
 var today =  moment();
-var test = $('.timeblock')
-var hour = $('#hour');
 var currentDay = $('#currentDay').text(today.format("dddd, MMMM Do"));
 var successMsg = $('#saveMessage');
 savedTimer = 5;
 
-// Render localstorage values
-$('#input1').text(localStorage.getItem('9'));
-$('#input2').text(localStorage.getItem('10'));
-$('#input3').text(localStorage.getItem('11'));
-$('#input4').text(localStorage.getItem('12'));
-$('#input5').text(localStorage.getItem('13'));
-$('#input6').text(localStorage.getItem('14'));
-$('#input7').text(localStorage.getItem('15'));
-$('#input8').text(localStorage.getItem('16'));
-$('#input9').text(localStorage.getItem('17'));
+init();
 
 $( ".saveBtn" ).on('click', function( event ) {
   successMsg.text("Appointment saved to localStorage")
@@ -32,11 +21,10 @@ function saveValue(){
     
     var hideSaved = setInterval(function () {
       savedTimer--;
-          console.log(savedTimer);
-          if (savedTimer <= 0) {
-            clearInterval(hideSaved);
-            successMsg.css('display', 'none') 
-          }
+        if (savedTimer <= 0) {
+          clearInterval(hideSaved);
+          successMsg.css('display', 'none') 
+        }
     }, 1000);
     var showSaved = setInterval(function () {
       successMsg.css('display', 'flex') 
@@ -58,25 +46,34 @@ function init() {
     var rowEl = $('<div class="row" id="' + hour + '">');
     rootEl.append(rowEl);
     //create the hour area
-    var timeEl = $('<div class="col-2 col-sm-2 col-md-2 col-lg-1 col-xl-1 timestamp">' + hour + amPm(hour) + '</div>')
-
+    var timeEl = $('<div class="col-2 col-sm-2 col-md-2 col-lg-1 col-xl-1 timestamp">' + hour + amPm(hour) + '</div>');
+    rowEl.append(timeEl)
+    //create textarea
+    var textAreaEl = $('<div class="col-8 col-sm-8 col-md-8 col-lg-10 col-xl-10 timeblock"><textarea></textarea></div>');
+    rowEl.append(textAreaEl);
     /*could be done diferently but just to practice the navigation truout the DOM in Jquerry 
     We add a class to change the row collor based on the hour */
     if(hour == today.hour()){
-      rowEl.children().eq(1).addClass('present'); 
+      textAreaEl.addClass('present'); 
     } else if(hour < today.hour()){
-      rowEl.children().eq(1).addClass('past');
+      textAreaEl.addClass('past');
     } else if(hour > today.hour()){
-      rowEl.children().eq(1).addClass('future');
+      textAreaEl.addClass('future');
     }
+
+    // Render localstorage values
+    textAreaEl.children().eq(0).text(localStorage.getItem(hour));
+    
+    //create button area
+    var buttonAreaEl = $('<div class="col-2 col-sm-2 col-md-2 col-lg-1 col-xl-1"><button class="saveBtn"><i class="fas fa-save fa-3x"></i></button></div>');
+    rowEl.append(buttonAreaEl);    
+
     function amPm(hour){
-      if(hour < 11){return "AM"
+      if(hour < 12){return "AM";
       } else return "PM";
     }
 });
 }
-
-init();
 
 /*
 <div class="row " id="14">
@@ -89,21 +86,4 @@ init();
   <div class="col-2 col-sm-2 col-md-2 col-lg-1 col-xl-1">
     <button class="saveBtn"><i class="fas fa-save fa-3x"></i></button>
   </div>
-</div>
-
-*/
-// Create Timeslots
-// create array with the hours
-// loop the array of hours to create the rows
-      
-//   create TAG for row/hour/input/button 
-
-//   give class to row/hour/input/button 
-
-//   create a dataset to keep the hour in the (row)
-
-//   style timeslots based on the hour comparing to "today.hour()"
-
-//     append all tags
-
-//   add event listener to add local storage when the save button is clicked in specific timeblock.(Data key "time")
+</div>*/
